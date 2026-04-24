e34ade9cdb176e967c0b0da7b8062441d731b942
Markuss-Berzins <markuss@markussberzins.com>
2026-04-24 16:52:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
  * VISU ATTĒLU SARAKSTS
  ***********************/
 const ALL_QUESTIONS = [
-  { image: "images/q01.png", phishing: true,  explanation: "Parasts bankas paziņojums, bez pielikumiem vai ļaunprātīgiem linkiem." },
+  { image: "images/q01.png", phishing: false,  explanation: "Parasts bankas paziņojums, bez pielikumiem vai ļaunprātīgiem linkiem." },
   { image: "images/q02.png", phishing: false, explanation: "Parasts paziņojums no veikala. Links ved uz leģitīmu veikala mājaslapu." },
   { image: "images/q03.png", phishing: true,  explanation: "Sola nepamatotu laimestu (50 EUR dāvanu karte). Sūtītāja e‑pasta adrese nav saistīta ar zināmu uzņēmumu. Steidzina lietotāju nekavējoties rīkoties. Aicina klikšķināt uz saites ārpus oficiālas vietnes. Iespējams mērķis - izkrāpt personas vai finanšu datus." },
-  { image: "images/q04.png", phishing: false, explanation: "Izmanto biedējošu brīdinājumu (IT kvota ir beigusies!), lai radītu paniku. Sūtītāja nosaukums Sistēmas Administrators ir neskaidrs un neidentificējams. E‑pasta adrese/domēns neizskatās pēc oficiāla uzņēmuma IT dienesta. Aicina steidzami klikšķināt uz saites (Palielināt Kvotu). Saites adrese (apakšā redzamā) ved uz aizdomīgu, neoficiālu tīmekļa vietni. Mērķis, visticamāk, ir iegūt piekļuves datus (kontam vai parolei)." },
+  { image: "images/q04.png", phishing: true, explanation: "Izmanto biedējošu brīdinājumu (IT kvota ir beigusies!), lai radītu paniku. Sūtītāja nosaukums Sistēmas Administrators ir neskaidrs un neidentificējams. E‑pasta adrese/domēns neizskatās pēc oficiāla uzņēmuma IT dienesta. Aicina steidzami klikšķināt uz saites (Palielināt Kvotu). Saites adrese (apakšā redzamā) ved uz aizdomīgu, neoficiālu tīmekļa vietni. Mērķis, visticamāk, ir iegūt piekļuves datus (kontam vai parolei)." },
   { image: "images/q05.png", phishing: true,  explanation: "Izmanto steidzamu paziņojumu (Jūsu rīcība ir nepieciešama!), lai izraisītu paniku. Apgalvo, ka sūtījums ir aizturēts muitā, kas ir bieži izmantots krāpnieku scenārijs. Pieprasa nelielu maksu (1,99 EUR), lai mazinātu aizdomas. Sūtītāja nosaukums Kurjera Dienests ir neskaidrs un nepārbaudāms. E‑pasta un saites domēns neatbilst reālam kurjerpasta uzņēmumam. Aicina klikšķināt uz pogas (Maksāt Nodevu Tūlīt). Saite ved uz aizdomīgu ārēju vietni, kur var tikt izkrāpti maksājumu dati." },
   { image: "images/q06.png", phishing: true,  explanation: "Nepazīstams sūtītājs." },
   { image: "images/q07.png", phishing: false, explanation: "Iekšējs paziņojums bez steigas." },
